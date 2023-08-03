981f05668507e50db49c6e29e562dd72e91da496
RinorSherifi0 <rinorsherifi.cr12@gmail.com>
2023-08-03 02:53:10 +0200

    Changes

---
+++ redirect_config.php
@@ -0,0 +1,15 @@
+<?php
+$redirects = array(
+    array(
+        'url' => '',
+        'date' => '2023-08-01',
+        'time' => '12:00:00'
+    ),
+    array(
+        'url' => 'https://example.com/destination2',
+        'date' => '2023-08-03',
+        'time' => '15:30:00'
+    ),
+    // Add more redirections as needed
+);
+?>
